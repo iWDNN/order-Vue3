4a25251b5165cad827554e4e2295afc01838bc11
iWDNN <cksgh2064@daum.net>
2023-03-01 19:46:58 +0900

    readme

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,82 +1,81 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import VueCookies from 'vue-cookies'
-import store from '~/store'
+import { createRouter, createWebHashHistory } from "vue-router";
+import VueCookies from "vue-cookies";
+import store from "~/store";
 // main
-import Management from './Management'
-import Login from './Login'
-import SignUp from './SignUp'
+import Management from "./Management";
+import Login from "./Login";
+import SignUp from "./SignUp";
 
 // children
-import Main from '~/components/Main'
-import MenuList from '~/components/MenuList'
-import TableList from '~/components/TableList'
-import OrderList from '~/components/OrderList'
-
+import Main from "~/components/Main";
+import MenuList from "~/components/MenuList";
+import TableList from "~/components/TableList";
+import OrderList from "~/components/OrderList";
 
 export default createRouter({
   history: createWebHashHistory(),
   scrollBehavior() {
     return {
-      top: 0
-    }
+      top: 0,
+    };
   },
   routes: [
     {
-      path: '/',
+      path: "/",
       beforeEnter: (to, from, next) => {
-        let isLoginObj = store.getters['login/getisLogin']
+        let isLoginObj = store.getters["login/getisLogin"];
         if (isLoginObj.isLogin) {
-          next({ path: '/manage' })
+          next({ path: "/manage" });
         } else {
-          next({ path: '/login' })
-        }
-      }
-    },
-    {
-      path: '/login',
-      component: Login,
-      beforeEnter: (to, from, next) => {
-        let actoken = VueCookies.get("accessToken")
-        if (actoken) {
-          next({ path: '/manage/main' })
-        } else {
-          next()
+          next({ path: "/login" });
         }
       },
     },
     {
-      path: '/sign_up',
+      path: "/login",
+      component: Login,
+      beforeEnter: (to, from, next) => {
+        let actoken = VueCookies.get("accessToken");
+        if (actoken) {
+          next({ path: "/manage/main" });
+        } else {
+          next();
+        }
+      },
+    },
+    {
+      path: "/sign_up",
       component: SignUp,
     },
     {
-      path: '/manage',
+      path: "/manage",
       component: Management,
       beforeEnter: (to, from, next) => {
-        let isLoginObj = store.getters['login/getisLogin']
+        let isLoginObj = store.getters["login/getisLogin"];
         if (isLoginObj.isLogin) {
-          next()
+          next();
         } else {
-          next({ path: '/login' })
+          next({ path: "/login" });
         }
       },
       children: [
         {
-          path: 'main',
-          component: Main
+          path: "main",
+          component: Main,
         },
         {
-          path: 'menu',
-          component: MenuList
+          path: "menu",
+          component: MenuList,
         },
         {
-          path: 'table',
-          component: TableList
+          path: "table",
+          component: TableList,
         },
         {
-          path: 'order',
-          component: OrderList
+          path: "order",
+          component: OrderList,
         },
-      ]
-    }
-  ]
-})
+      ],
+    },
+  ],
+});
